ac7f56877cbce87d2e0f6f516e011b8283a93b8b
khoinguyen140612 <khoinguyen140612@gmail.com>
2025-10-31 08:49:40 +0700

    Update index.js manual status

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ import { ActivityType } from "discord.js";
 // Manual / Auto setup
 let manualOverride = false;
 let manualStatus = {
-  name: 'đang ngủ đừng làm phiền Kaoruko🌙 (dev-nguyenbui1406)',
+  name: 'đi chơi cùng dev-nguyenbui1406!',
   type: ActivityType.Playing,
   status: 'dnd'
 };
@@ -379,6 +379,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Kaoruko đang hoạt động! 💖"));
 app.listen(3000, () => console.log("🌐 Web server chạy ở cổng 3000 để giữ bot online"));
+
 
 
 
